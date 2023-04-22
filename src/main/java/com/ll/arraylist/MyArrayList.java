4efd9fedd1bc2baed10d19eb5746358ea878c7af
김동우 <nanaukim@naver.com>
2023-04-22 20:02:33 +0900

    배열의 크기가 부족할 때 자동으로 2배 증가하는지 확인

--- src/main/java/com/ll/arraylist/MyArrayList.java
+++ src/main/java/com/ll/arraylist/MyArrayList.java
@@ -1,12 +1,42 @@
 package com.ll.arraylist;
 
 public class MyArrayList<T> {
+    private int size = 0;
+    private String[] data = new String[2];
+
     public int size() {
-        return 0;
+        return size;
     }
 
-    public boolean add(String el) {
-        this.add(el);
+    public boolean add(T element) {
+        makeNewDataIfNotEnough();
+        data[size] = (String) element;
+        size++;
         return true;
+    }
+
+    private void makeNewDataIfNotEnough() {
+        if (ifNotEnough()) {
+            makeNewData();
+        }
+    }
+
+    private void makeNewData() {
+        String[] newData = new String[data.length * 2];
+
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
+
+    private boolean ifNotEnough() {
+        return size >= data.length;
+    }
+
+    public void remove(T element) {
+        size--;
+    }
+
+    public String get(int idx) {
+        return data[idx];
     }
 }
--- src/test/java/com/ll/arraylist/MyArrayListTest.java
+++ src/test/java/com/ll/arraylist/MyArrayListTest.java
@@ -1,117 +1,64 @@
 package com.ll.arraylist;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyArrayListTest {
-    private MyArrayList<String> list;
-
-    @BeforeEach
-    void setUp() {
-        list = new MyArrayList<>();
-    }
 
     @Test
-    void testSize() {
+    @DisplayName("사이즈 테스트")
+    void t1() {
         MyArrayList<String> list = new MyArrayList<>();
-        assertEquals(0, list.size());
+        assertThat(list.size()).isEqualTo(0);
     }
 
     @Test
-    void testAdd() {
-        assertTrue(list.add("Element1"));
-        assertEquals(1, list.size());
-//        assertEquals("Element1", list.get(0));
+    @DisplayName("add(\"사과\")")
+    void t2() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과"); // data[0]
+        list.add("포도"); // data[1];
+
+        assertThat(list.size()).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("get(1)")
+    void t3() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과"); // data[0]
+        list.add("포도"); // data[1];
+
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.get(1)).isEqualTo("포도");
+    }
+
+    @Test
+    @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
+    void t4() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+        list.add("당근");
+
+        assertThat(list.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("배열 크기를 한계에 계속 늘림")
+    void t5() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        assertThat(list.size()).isEqualTo(100);
+    }
+
 }
-
-/*
-
-
-
-
-    @Test
-    void testRemove() {
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
-        assertEquals("Element2", list.remove(1));
-        assertEquals(2, list.size());
-    }
-
-    @Test
-    void testGet() {
-        list.add("Element1");
-        list.add("Element2");
-        assertEquals("Element1", list.get(0));
-        assertEquals("Element2", list.get(1));
-    }
-
-    @Test
-    void testSize() {
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
-        assertEquals(3, list.size());
-    }
-
-    @Test
-    void testContains() {
-        list.add("Element1");
-        list.add("Element2");
-        assertTrue(list.contains("Element1"));
-        assertFalse(list.contains("Element3"));
-    }
-
-    @Test
-    void testIndexOf() {
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element1");
-        assertEquals(0, list.indexOf("Element1"));
-        assertEquals(1, list.indexOf("Element2"));
-        assertEquals(-1, list.indexOf("Element3"));
-    }
-
-
-    @Test
-    void testAddAtIndex() {
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
-        list.add(2, "Element4");
-        assertEquals(4, list.size());
-        assertEquals("Element1", list.get(0));
-        assertEquals("Element2", list.get(1));
-        assertEquals("Element4", list.get(2));
-        assertEquals("Element3", list.get(3));
-    }
-
-    @Test
-    void testSet() {
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
-        assertEquals("Element1", list.set(0, "Element4"));
-        assertEquals(3, list.size());
-        assertEquals("Element4", list.get(0));
-        assertEquals("Element2", list.get(1));
-        assertEquals("Element3", list.get(2));
-    }
-
-    @Test
-    void testClear() {
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
-        list.clear();
-        assertEquals(0, list.size());
-        assertTrue(list.isEmpty());
-    }
-
-    // 심화문제 1 : removeIf 메서드 구현
-    // list.removeIf(e -> e.equals("Element2"));
- */
