2a0c0e49d483c0264596928522affbb490d6bb92
김동우 <nanaukim@naver.com>
2023-04-22 19:06:50 +0900

    ArrayList를 만들기 위한 기초 뼈대와 이를 검증하기 위한 테스트파일 생성

--- src/main/java/com/ll/arraylist/MyArrayList.java
+++ src/main/java/com/ll/arraylist/MyArrayList.java
@@ -1,4 +1,12 @@
 package com.ll.arraylist;
 
-public class MyArrayList {
+public class MyArrayList<T> {
+    public int size() {
+        return 0;
+    }
+
+    public boolean add(String el) {
+        this.add(el);
+        return true;
+    }
 }
